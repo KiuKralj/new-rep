a9394978319b149c430ac6dd9eec21fe51a0c40d
KiuKralj <urosinter@gmail.com>
2024-11-29 13:02:39 +0100

    bit-show-update

--- React/React-Bit-show/src/components/Header/Header.jsx
+++ React/React-Bit-show/src/components/Header/Header.jsx
@@ -1,11 +1,42 @@
 import React from "react";
-import "./Header.css" 
+import "./Header.css";
+import { useEffect, useState } from "react";
 
 function Header() {
+
+  const [searchQuery, setSearchQuery] = useState(""); 
+  const [searchResults, setSearchResults] = useState([]);
+
+  const fetchSearchResults = (query) => {
+    if (query) {
+      fetch(`https://api.tvmaze.com/search/shows?q=${query}`)
+        .then((response) => response.json())
+        .then((data) => setSearchResults(data))
+    } else {
+      setSearchResults([]);  
+    }
+  };
+
+  useEffect(() => {
+    fetchSearchResults(searchQuery);
+  }, [searchQuery]);
+
+
   return (
     <header>
       <h1>BitShow</h1>
-      <input type='search' className="search-input"/>
+      <div className="search-wrapper">
+        <div>
+          <input type="search" className="search-input" 
+          onChange={(e) => setSearchQuery(e.target.value)}   />
+        </div>
+        {searchQuery && (
+          <div className="search-results">
+            
+          </div>
+        )}
+        
+      </div>
     </header>
   );
 }
--- React/React-Bit-show/src/pages/SinglePage/SinglePage.jsx
+++ React/React-Bit-show/src/pages/SinglePage/SinglePage.jsx
@@ -1,18 +1,72 @@
-import React from 'react'
+import React from "react";
 import "./SinglePage.css";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
-
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 
 export const SinglePage = ({ data }) => {
   const { id } = useParams();
+  const [seasons, setSeasons] = useState([]);
+  const [casts, setCasts] = useState([]);
 
   const show = data.find((show) => {
-    return show.id === id
-  })
+    return show.id === parseInt(id);
+  });
+
+  useEffect(() => {
+    const seasonsURL = `https://api.tvmaze.com/shows/${id}/seasons`;
+
+    fetch(seasonsURL)
+      .then((response) => response.json())
+      .then((data) => setSeasons(data));
+  },[]);
+
+  useEffect(() => {
+    const castURL = `https://api.tvmaze.com/shows/${id}/cast`;
+
+    fetch(castURL)
+      .then((response) => response.json())
+      .then((data) => setCasts(data));
+  },[]);
 
   return (
-    <div className='single-page'>
-      <h1>Cao</h1>
-    </div>
-  )
-}
+    <>
+      <Header />
+      <div className="single-page">
+        <div className="single-page-show-name">
+          <h1>{show?.name}</h1>
+        </div>
+        <div className="wrapper">
+          <div className="single-page-image">
+            <img src={show?.image.medium} alt="show" className="show-image" />
+          </div>
+          <div className="seasons">
+            <h2>{`Season (${seasons.length})`}</h2>
+            <ul>
+              {seasons.map((season) => (
+                <li>
+                  {season.premiereDate} - {season.endDate}
+                </li>
+              ))}
+            </ul>
+          </div>
+          {/* <div className="casts">
+            <h2>Cast</h2>
+            <ul>
+              {casts.map((cast) => (
+                <li key={cast.person.id}>
+                  {cast.person.name}
+                </li>
+              ))}
+            </ul>
+          </div> */}
+        </div>
+        <div className="single-page-summary">
+          <p dangerouslySetInnerHTML={{ __html: show?.summary }} />
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
